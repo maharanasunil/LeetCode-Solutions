dd220c8d9a59fba573fbd9cb518ad0795944f929
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-06-11 00:22:40 +0530

    Time: 947 ms (97.79%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -6,3 +6,15 @@ WHERE s.sales_id NOT IN
  JOIN Company c USING(com_id)
  WHERE o.com_id = c.com_id AND c.name = 'RED'
 )
+
+/*
+Explaination:
+The first inner join creates a view for just 'RED' orders
+The right join ensures all salespersons are included (even those who do not have RED orders)
+The WHERE IS NULL gives salespersons who did not have any RED orders due to the right join
+
+select salesperson.name
+from orders o join company c on (o.com_id = c.com_id and c.name = 'RED')
+right join salesperson on salesperson.sales_id = o.sales_id
+where o.sales_id is null
+*/
