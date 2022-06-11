522f2fc43c9472ccd3c5b8348aff686552681231
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-06-11 12:37:13 +0530

    Time: 521 ms (40.40%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,22 @@
 # Write your MySQL query statement below
+# The below solution is for the follow up question.
+
+WITH CTE AS
+    (
+        SELECT customer_number, RANK() OVER(ORDER BY COUNT(1) DESC) AS RANK_val
+        FROM Orders 
+        GROUP BY customer_number
+    )
+SELECT customer_number
+FROM CTE
+WHERE RANK_val = 1;
+
+/*
 SELECT 
     customer_number 
-FROM Orders 
+FROM 
+    Orders 
 GROUP BY  customer_number
-ORDER BY COUNT(*) DESC LIMIT 1;
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+*/
