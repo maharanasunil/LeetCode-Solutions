bb413d1f34978001a9e5d44210d2eb8a5e78e20f
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-06-11 12:17:46 +0530

    Time: 398 ms (87.85%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -3,12 +3,14 @@ SELECT
     activity_date AS day,
     COUNT(DISTINCT user_id) AS active_users
 FROM Activity 
-group by activity_date having activity_date between '2019-06-28' and '2019-07-27'
-
-/*
 WHERE 
-    DATEDIFF('2019-07-27',activity_date) < 30
+    DATEDIFF('2019-07-27', activity_date) < 30
+    AND activity_date <= '2019-07-27'
 GROUP BY
     activity_date
+
+/*
+GROUP BY activity_date 
+HAVING activity_date BETWEEN '2019-06-28' and '2019-07-27'
 */
 #  Datediff(date1, date2) = date1 - date2
