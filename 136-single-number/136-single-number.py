aa4bbb338f9c3adeccb61f76f9da06fb38184ea1
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-06-25 22:22:13 +0530

    Time: 181 ms (63.66%), Space: 16.8 MB (20.43%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -2,5 +2,10 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         ans = 0
         for num in nums:
-            ans = ans^num
+            ans = ans^num # XOR of the entire Array
         return ans
+
+#Properties of XOR
+#1: a^0 = a
+#2: a^a = a
+#3: a^b = b^a
