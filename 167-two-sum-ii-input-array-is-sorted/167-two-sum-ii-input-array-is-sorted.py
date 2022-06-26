21bfc576d94d10807eb632fa73ec09857cd5b90c
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-06-26 22:33:23 +0530

    Time: 135 ms (91.26%), Space: 14.9 MB (88.11%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        f = 0
-        s = len(nums) - 1
-        while(f < s):
-            currentSum = nums[f] + nums[s]
+        left_pointer = 0
+        right_pointer = len(nums) - 1
+        while(left_pointer < right_pointer):
+            currentSum = nums[left_pointer] + nums[right_pointer]
             if (currentSum < target):
-                f += 1
+                left_pointer += 1
             elif (currentSum > target):
-                s -= 1
+                right_pointer -= 1
             else:
-                return [f+1, s+1]
+                return [left_pointer+1, right_pointer+1]
+        return []
