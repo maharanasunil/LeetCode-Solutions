7a8bc5144ab8a9ad584dc03f4e87423e286c09bf
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-06-28 21:48:17 +0530

    Time: 61 ms (11.36%), Space: 13.9 MB (56.62%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,29 @@
+#Brute Force Approach. Takes a lot of Memory!
+#class Solution:
+#    def climbStairs(self, n: int) -> int:
+#        return self.f(0, n)
+#        
+#    def f(self, i, n):
+#        if (i > n):
+#            return 0
+#        elif (i == n):
+#            return 1
+#        else:
+#            return self.f(i+1, n) + self.f(i+2, n)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        self.ht = {}
+        return self.f(0, n)
+        
+    def f(self, i, n):
+        if (i > n):
+            return 0
+        elif (i == n):
+            return 1
+        else:
+            if i in self.ht:
+                return self.ht[i]
+            else:
+                self.ht[i] = self.f(i+1, n) + self.f(i+2, n)
+                return self.ht[i]
