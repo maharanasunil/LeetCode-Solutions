08f0576dcb44cb816ba2a64521f82b48faef0e08
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-06-29 20:52:50 +0530

    Time: 245 ms (16.98%), Space: 20.8 MB (28.83%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,13 +1,21 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        result = []
-        for i in range(n+1):
-            result.append(self.getNoOfSetBits(i))
+        result = [0]
+        for i in range(1, n+1):
+            t = str(bin(i)).replace("0b", "")
+            t = list(t)
+            result.append(t.count("1"))
         return result
-        
-    def getNoOfSetBits(self, num: int) -> int:
-        count = 0
-        for i in range(32): 
-            if((num & (1 << i)) > 0):
-                count += 1
-        return count
+                
+#        result = []
+#        for i in range(n+1):
+#            result.append(self.getNoOfSetBits(i))
+#        return result
+#        
+#    def getNoOfSetBits(self, num: int) -> int:
+#        count = 0
+#        for i in range(32): 
+#            if((num & (1 << i)) > 0):
+#                count += 1
+#        return count
+
