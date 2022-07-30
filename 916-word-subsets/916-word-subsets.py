b12d26e3ee755bbc99f8564b6323b7514292a5f1
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-07-30 09:34:25 +0530

    Time: 1911 ms (16.01%), Space: 18.8 MB (23.33%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
+        freq = [0]*26 
+        
+        for w in B: 
+            temp = [0]*26
+            for c in w: temp[ord(c)-97] += 1
+            for i in range(26): freq[i] = max(freq[i], temp[i])
+                
+        ans = []
+        for w in A: 
+            temp = [0]*26
+            for c in w: temp[ord(c)-97] += 1
+            if all(freq[i] <= temp[i] for i in range(26)): ans.append(w)
+        return ans 
+class Solution:
+    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
+        freq = Counter()
+        for x in B: freq |= Counter(x)
+        return [x for x in A if not freq - Counter(x)]
