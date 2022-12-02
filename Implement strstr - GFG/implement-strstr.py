d8c3254a149ba2bc9a71662dde8873024e19e862
Sunil Maharana <64277960+maharanasunil@users.noreply.github.com>
2022-12-02 18:02:40 +0530

    Added solution - LeetHub

---
+++ Implement strstr - GFG/implement-strstr.py
@@ -0,0 +1,36 @@
+#Function to locate the occurrence of the string x in the string s.
+def strstr(s,x):
+    result = s.find(x)
+    if result != -1:
+        return result
+    else:
+        return -1
+    #code here
+
+
+#{ 
+ # Driver Code Starts
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        s,p =input().strip().split()
+        print(strstr(s,p))
+
+
+# } Driver Code Ends
